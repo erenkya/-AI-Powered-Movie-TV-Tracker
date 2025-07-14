54336ee15564fe0829a06010b9f779969fbb0c85
Eren Kaya <19soft1096@isik.edu.tr>
2025-07-14 19:37:39 +0300

    Structuring the project foldur according to React Frontend and Spring Boot Backend

---
+++ -AI-Powered-Movie-TV-Tracker Backend/src/main/java/com/_AI_Powered_Movie_TV_Tracker/_AI_Powered_Movie_TV_Tracker/AiPoweredMovieTvTrackerApplication.java
@@ -0,0 +1,13 @@
+package com._AI_Powered_Movie_TV_Tracker._AI_Powered_Movie_TV_Tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiPoweredMovieTvTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiPoweredMovieTvTrackerApplication.class, args);
+	}
+
+}
